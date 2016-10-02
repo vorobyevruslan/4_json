62648a9473843e77796d0ff7e719240702235a52
Ruslan Vorobyev <bodyrof1@gmail.com>
2016-10-03 01:45:08 +0300

    Updated code

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
-
+import pprint
 
 def load_data(filepath):
-    pass
+    strin = ''
+    with open(filepath, encoding='utf8') as iner:
+        for line in iner:
+            strin += line
+    return strin
 
 
 def pretty_print_json(data):
-    pass
+    pprint.pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input()
+    strin = load_data(filepath)
+    data = json.loads(strin)
+    pretty_print_json(data)
